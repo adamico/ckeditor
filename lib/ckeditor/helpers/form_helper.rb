f99a8c9265ffb59d690fceac0fcc20109bef5f33
Andrea D'Amico <andrea.damico@gmail.com>
2013-07-18 14:00:55 +0200

    tentative rails 4 compatibility fix

--- lib/ckeditor/helpers/form_helper.rb
+++ lib/ckeditor/helpers/form_helper.rb
@@ -2,25 +2,25 @@ module Ckeditor
   module Helpers
     module FormHelper
       extend ActiveSupport::Concern
-      
+
       include ActionView::Helpers::TagHelper
       include ActionView::Helpers::JavaScriptHelper
-      
+
       def cktext_area(object_name, method, options = {})
         options = (options || {}).stringify_keys
         ck_options = (options.delete('ckeditor') || {}).stringify_keys
         output = ""
-        
-        if defined?(ActionView::Base::InstanceTag)
-          instance_tag = ActionView::Base::InstanceTag.new(object_name, method, self, options.delete('object'))
-          instance_tag.send(:add_default_name_and_id, options) if options['id'].blank?
-          output = instance_tag.to_text_area_tag(options)
-        else
+
+        if defined?(ActionView::Helpers::Tags::TextArea)
           instance_tag = ActionView::Helpers::Tags::TextArea.new(object_name, method, self, options)
           instance_tag.send(:add_default_name_and_id, options) if options['id'].blank?
           output = instance_tag.render
+        else
+          instance_tag = ActionView::Base::InstanceTag.new(object_name, method, self, options.delete('object'))
+          instance_tag.send(:add_default_name_and_id, options) if options['id'].blank?
+          output = instance_tag.to_text_area_tag(options)
         end
-        
+
         output_buffer = ActiveSupport::SafeBuffer.new
         output_buffer << output
         output_buffer << javascript_tag(Utils.js_replace(options['id'], ck_options))
